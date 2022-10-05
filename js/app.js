b10ee487306dd4a9bd80b04b452def4c8edba61a
AzAlex <azalexxx02@gmail.com>
2022-10-04 18:31:28 -0700

    ++

--- js/app.js
+++ js/app.js
@@ -1,33 +1,34 @@
-const SliderMain = new Swiper('.slider-main', {
-  // freeMode: true, //слайд зупиняжться там де його відпускають
-  centeredSlides: true,//головний слайд по центру
-  parallax: true,
-  // slidesPerView: 3, //кількість показаних слайдів
-  breakpoints: {
-    0: {//від до безкінечності або аступного брекпоінту
-      slidesPerView: 2.5,
-      spaceBetween: 20, //відстань між слайдами
-    },
-    680: {//від до безкінечності або аступного брекпоінту
-      slidesPerView: 3.5,
-      spaceBetween: 60, //відстань між слайдами
-    }
-  }
+const sliderMain = new Swiper('.slider_main', {
+	// freeMode: true,
+	centeredSlides: true,
+	mousewheel: true,
+	parallax: true,
+	breakpoints: {
+		0: {
+			slidesPerView: 2.5,
+			spaceBetween: 20
+		},
+		680: {
+			slidesPerView: 3.5,
+			spaceBetween: 60
+		}
+	}
 })
-const SliderBg = new Swiper('.slider-bg', {
-  // freeMode: true, //слайд зупиняжться там де його відпускають
-  centeredSlides: true,//головний слайд по центру
-  parallax: true,
-  slidesPerView: 3.5,
-  spaceBetween: 60,
+const sliderBg = new Swiper('.slider_bg', {
+	centeredSlides: true,
+	parallax: true,
+	spaceBetween: 60,
+	slidesPerView: 3.5
 })
-SliderMain.controller.control = SliderBg
-document.querySelectorAll('.slider-item').forEach(item => {
-  item.addEventListener('click', event => {
-    item.classList.toggle('opened')
-  })
+sliderMain.controller.control = sliderBg
+
+document.querySelectorAll('.slider__item').forEach(item => {
+	item.addEventListener('click', event => {
+		item.classList.toggle('opened')
+	})
 })
-let desc = document.querySelector('.description');
-SliderMain.on('slideChange', e => {
-  SliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
+
+let desc = document.querySelector('.description')
+sliderMain.on('slideChange', e => {
+	sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
 })
